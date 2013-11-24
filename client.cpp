6696def1e9bcf21ac9d52e749606554db1fbe2a3
James Palawaga <vsfmag@gmail.com>
2013-11-24 15:31:00 -0700

    Receive seems to work

--- client.cpp
+++ client.cpp
@@ -9,6 +9,9 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <curses.h>
+#include <thread>         // std::thread, std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include <deque>
 
 using namespace std;
 
@@ -22,6 +25,8 @@ static const char ACK_RESPONSE = '3';
 //
 // 0=JOIN, 1=GET, 2=SEND, 3=ACK
 
+
+
 void error(string msg)
 {
     cout << msg;
@@ -31,9 +36,67 @@ void error(string msg)
 
 static int current_sequence_number = 1;
 static char buffer[256];
+static bool output_unlocked = true;
+
+class WindowManager {
+
+private:
+    deque<string> screenbuffer;
+    WINDOW * window;
+    thread threaded;
+
+public:
+    static void drawScreenBuffer(WINDOW * test, deque<string> * sb) {
+        while(1) {
+            std::this_thread::sleep_for (std::chrono::microseconds(100));
+            if (output_unlocked) {
+                wclear(test);
+                for (int i = sb->size(); i > 0; i--) {
+                    mvwprintw(test,i-1, 0, (char*)sb->at(i-1).c_str());
+                }
+                wrefresh(test);
+            }
+        }
+    }
+    WindowManager() {
+        initscr();
+        refresh();
+        window = newwin(23, 94, 0, 0);
+        threaded = thread(drawScreenBuffer, window, &screenbuffer);
+        threaded.detach();
+    }
+
+    void write(string message) {
+        output_unlocked = false;
+        if (screenbuffer.size() >= 23) {
+            screenbuffer.pop_front();
+        }
+        screenbuffer.push_back(message);
+        output_unlocked = true;
+    }
+
+    void end() {
+        output_unlocked = false;
+        endwin();
+    }
+
+    void requestInput(char* prompt, char* input) {
+        char str[16];
+        mvprintw(23, 0, "%s", prompt);
+                            /* print the message at the center of the screen */
+        getstr(str);
+        input = str;
+    }
+
+    void refresh() {
+        wrefresh(window);
+    }
+};
 
 int main(int argc, char *argv[])
 {
+    WindowManager winMan = WindowManager();
+
     int portno = 9009;
     int sockfd, n;
 
@@ -58,19 +121,18 @@ int main(int argc, char *argv[])
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
-    n = write(sockfd,"000",strlen(buffer));
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-
     while(1) {
-        printf("\n\nWhat would you like to do?\n");
-        printf("[r]eceive or [s]end: ");
-
-        char selection[3];
-        fgets(selection, 2, stdin);
-        strtok(selection, "\n");
+        char selection[1];
+        char prompt[95];
+        strncpy(prompt, "                                                            ", 94);
+        mvprintw(24, 0, "%s", prompt);
+        winMan.refresh();
+        strncpy(prompt, "[R]eceive | [S]end | e[X]it $> ", 94);
+        mvprintw(24, 0, "%s", prompt);
+        winMan.refresh();
+                            /* print the message at the center of the screen */
+        getstr(selection);
+        char buffer2[100];
 
         switch(selection[0]) {
             case 'r':
@@ -82,12 +144,14 @@ int main(int argc, char *argv[])
                     n = read(sockfd,buffer,255);
 
                     if (buffer[1] == ACK_RESPONSE) {
-                        printf("No more messages to retreive!");
+                        winMan.write("No more messages to retreive!");
                         break;
                     }
 
-                    printf("\nMessage From: %s\n",buffer+2);
-                    printf("Message Received: %s\n",buffer+18);
+                    sprintf(buffer2, "\nMessage Frodm: %s\n",buffer+2);
+                    winMan.write(buffer2);
+                    sprintf(buffer2, "Message Received: %s\n",buffer+18);
+                    winMan.write(buffer2);
 
                     bzero(buffer+2, 254);
                     buffer[1] = '3';
@@ -101,13 +165,13 @@ int main(int argc, char *argv[])
                 buffer[1] = '2';
 
                 // Get destination IP address
-                printf("Please enter the recipient's IP: ");
+                winMan.write("Please enter the recipient's IP: ");
                 fgets(buffer+2,16,stdin);
                 // Bit crazy here: Remove any newline characters (NO NEW LINES PLZ)
                 strtok(buffer+2, "\n");
 
                 // Get message
-                printf("Please enter the message: ");
+                winMan.write("Please enter the message: ");
                 fgets(buffer+18,248,stdin);
                 strtok(buffer+18, "\n");
 
@@ -120,13 +184,17 @@ int main(int argc, char *argv[])
                      error("ERROR reading from socket");
 
                 if (((int)(buffer[0]) - 48) == current_sequence_number && buffer[1] == ACK_RESPONSE) {
-                    printf("ACK successfully recieved.");
+                    winMan.write("ACK successfully recieved.");
                 } else {
-                    printf("Server error occured...");
+                    winMan.write("Server error occured...");
                 }
                 break;
+
+                case 'x':
+                    winMan.end();
+                    return 0;
             default:
-                printf("invalid option!");
+                winMan.write("invalid option!");
                 break;
         }
     }
