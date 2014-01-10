a226ace0dd98ff6b6671fb1dd0c593059915b253
James Palawaga <vsfmag@gmail.com>
2014-01-10 14:26:24 -0700

    Window Manager split from client

---
+++ WindowManager.h
@@ -0,0 +1,69 @@
+#ifndef __WINDOWMANAGER_H
+#define __WINDOWMANAGER_H
+
+#include <deque>
+#include <curses.h>
+#include <strings.h>
+
+class WindowManager {
+
+  private:
+    std::deque<std::string> screenbuffer;
+    WINDOW * window;
+
+  public:
+    void drawScreenBuffer() {
+        wclear(window);
+        for (int i = screenbuffer.size(); i > 0; i--) {
+            mvwprintw(window, i-1, 0, (char*)screenbuffer.at(i-1).c_str());
+        }
+        wrefresh(window);
+    }
+
+    WindowManager() {
+        initscr();
+        refresh();
+        window = newwin(22, 94, 0, 0);
+    }
+
+    void write(std::string message) {
+        if (screenbuffer.size() >= 22) {
+            screenbuffer.pop_front();
+        }
+        screenbuffer.push_back(message);
+        drawScreenBuffer();
+    }
+
+    void end() {
+        endwin();
+    }
+
+    void requestInput(std::string prompt, char* input, int length) {
+
+        clearUserSpace();
+        mvprintw(23, 0, "%s", (char*)prompt.c_str());
+        getnstr(input, length);
+        strtok(input, "\n");
+        clearUserSpace();
+    }
+
+    void clearline(int line) {
+        int y,x;
+        getyx(window, y, x);
+        move(line, 0);
+        clrtoeol();
+        move (y,x);
+        wrefresh(window);
+    }
+
+    void clearUserSpace() {
+        clearline(23);
+        clearline(24);
+    }
+
+    void refresh() {
+        wrefresh(window);
+    }
+};
+
+#endif
--- client.cpp
+++ client.cpp
@@ -12,6 +12,7 @@
 #include <thread>         // std::thread, std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
 #include <deque>
+#include "WindowManager.h"
 
 using namespace std;
 
@@ -28,67 +29,6 @@ void error(string msg)
     cout << msg;
     exit(0);
 }
-
-class WindowManager {
-
-  private:
-    deque<string> screenbuffer;
-    WINDOW * window;
-
-  public:
-    void drawScreenBuffer() {
-        wclear(window);
-        for (int i = screenbuffer.size(); i > 0; i--) {
-            mvwprintw(window, i-1, 0, (char*)screenbuffer.at(i-1).c_str());
-        }
-        wrefresh(window);
-    }
-
-    WindowManager() {
-        initscr();
-        refresh();
-        window = newwin(22, 94, 0, 0);
-    }
-
-    void write(string message) {
-        if (screenbuffer.size() >= 22) {
-            screenbuffer.pop_front();
-        }
-        screenbuffer.push_back(message);
-        drawScreenBuffer();
-    }
-
-    void end() {
-        endwin();
-    }
-
-    void requestInput(string prompt, char* input, int length) {
-
-        clearUserSpace();
-        mvprintw(23, 0, "%s", (char*)prompt.c_str());
-        getnstr(input, length);
-        strtok(input, "\n");
-        clearUserSpace();
-    }
-
-    void clearline(int line) {
-        int y,x;
-        getyx(window, y, x);
-        move(line, 0);
-        clrtoeol();
-        move (y,x);
-        wrefresh(window);
-    }
-
-    void clearUserSpace() {
-        clearline(23);
-        clearline(24);
-    }
-
-    void refresh() {
-        wrefresh(window);
-    }
-};
 
 void checkForReadReceipts(WindowManager * winMan, int sockfd) {
 
